5b0e9dda1e7eaafa65cdf33c86195f81352e0ab5
Yudi Yamane <yudiyamane@gmail.com>
2022-01-04 15:28:10 -0300

    basic pwa

---
+++ README.md
@@ -0,0 +1,3 @@
+# PWA super basicão
+
+Seguindo esse [tutorial](https://www.loginradius.com/blog/async/build-pwa-using-vanilla-javascript/).
---
+++ sw.js
@@ -0,0 +1,22 @@
+let cacheName = 'pwa-basic'
+let filesToCache = [
+  '/',
+  'index.html',
+  '/css/style.css',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})
